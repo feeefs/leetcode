5bc6f0b5885b80bc394814f6f68bdcd3b26df553
feeefs <125202249+feeefs@users.noreply.github.com>
2023-10-11 23:26:58 +0300

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,6 @@
 // Both time and space complexity are equivalent to O(n) in the following solution. 
 class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] solve(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int remainder = target - nums[i];
