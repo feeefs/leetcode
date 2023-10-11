1fd7537103fedba1344b3ce74d62c76229a83b04
feeefs <125202249+feeefs@users.noreply.github.com>
2023-10-11 23:25:02 +0300

    Update and rename FinalValueOfVariableAfterPerformingOperation to FinalValueOfVariableAfterPerformingOperation.java

---
+++ FinalValueOfVariableAfterPerformingOperation.java
@@ -0,0 +1,8 @@
+class FinalValueOfVariableAfterPerformingOperation {
+        // Complexity Analysis: Time Complxity --> O(N) | Space Complexity --> O(1)
+        public int solve(String[] operations) {
+                int x =0;
+                for (String operation : operations) x += (44 - operation.charAt(1));
+                return x;
+            }
+}
